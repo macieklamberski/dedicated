cb187a8ac5f6e6a66a4e08c93394d2e301b1fff3
Maciej Lamberski <maciej@lamberski.com>
2011-09-09 21:04:40 +0200

    Updated views to apply changes made in Padamini

--- views/hint/site.php
+++ views/hint/site.php
@@ -1,6 +1,5 @@
 <?php foreach ($messages as $message): ?>
   <div class="message message-<?php echo $message['type'] ?>">
     <p><?php echo $message['text'] ?></p>
-    <a href="#" class="close"><?php echo __('Zamknij') ?></a>
   </div>
 <?php endforeach ?>
