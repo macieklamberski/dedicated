a177f8066f3e6b08f99f5b79ca7bf49e52a54ed5
Maciej Lamberski <maciej@lamberski.com>
2011-09-20 18:41:10 +0200

    Changed spaces to tabs in file

--- classes/controller/cms/admin/backups.php
+++ classes/controller/cms/admin/backups.php
@@ -2,97 +2,97 @@
 
 class Controller_CMS_Admin_Backups extends Controller_Admin {
 
-  protected $prefix;
+	protected $prefix;
 
-  public function before()
-  {
-    parent::before();
+	public function before()
+	{
+		parent::before();
 
-    require Kohana::find_file('vendor', 'Archive/Tar');
+		require Kohana::find_file('vendor', 'Archive/Tar');
 
-    $this->prefix = URL::title(Settings::get('project_name')).'-';
+		$this->prefix = URL::title(Settings::get('project_name')).'-';
 
-    // Deleting old backup files
-    $backups = array_merge(
-      glob(APPPATH.'cache/'.$this->prefix.'files-*'),
-      glob(APPPATH.'cache/'.$this->prefix.'database-*')
-    );
+		// Deleting old backup files
+		$backups = array_merge(
+			glob(APPPATH.'cache/'.$this->prefix.'files-*'),
+			glob(APPPATH.'cache/'.$this->prefix.'database-*')
+		);
 
-    foreach ($backups as $backup)
-    {
-    	@unlink($backup);
-    }
-  }
+		foreach ($backups as $backup)
+		{
+			@unlink($backup);
+		}
+	}
 
 	public function action_index() {}
 
 	public function action_database()
 	{
-    $tables = DB::query(Database::SELECT, 'SHOW TABLES')->execute()->as_array();
+		$tables = DB::query(Database::SELECT, 'SHOW TABLES')->execute()->as_array();
 
-    // Create CSV file from each table data
-    foreach ($tables as $table)
-    {
-      $table = reset($table);
-    	$file = APPPATH.'cache/'.$table.'.csv';
-    	$files[] = $file;
+		// Create CSV file from each table data
+		foreach ($tables as $table)
+		{
+			$table = reset($table);
+			$file = APPPATH.'cache/'.$table.'.csv';
+			$files[] = $file;
 
-    	$file = fopen($file, 'w');
+			$file = fopen($file, 'w');
 
-      $table_content = DB::query(Database::SELECT, 'SELECT * FROM '.$table)->execute()->as_array();
+			$table_content = DB::query(Database::SELECT, 'SELECT * FROM '.$table)->execute()->as_array();
 
-    	foreach ($table_content as $record)
-    	{
-    		fputcsv($file, $record);
-    	}
+			foreach ($table_content as $record)
+			{
+				fputcsv($file, $record);
+			}
 
-    	unset($table_content);
-    	fclose($file);
-    }
+			unset($table_content);
+			fclose($file);
+		}
 
-    // Create archive file from all CSV files
-    $archive_file = APPPATH.'cache/'.$this->prefix.'database-'.date('Y-m-d-H.i.s').'.tar.gz';
-    $archive = new Archive_Tar($archive_file, 'gz');
-    $archive->create($files);
+		// Create archive file from all CSV files
+		$archive_file = APPPATH.'cache/'.$this->prefix.'database-'.date('Y-m-d-H.i.s').'.tar.gz';
+		$archive = new Archive_Tar($archive_file, 'gz');
+		$archive->create($files);
 
-    // Delete CSV files
-    foreach ($files as $file)
-    {
-      @unlink($file);
-    }
+		// Delete CSV files
+		foreach ($files as $file)
+		{
+			@unlink($file);
+		}
 
-    // Send file to download
-    $this->response->send_file($archive_file);
+		// Send file to download
+		$this->response->send_file($archive_file);
 	}
 
 	public function action_files()
 	{
-    // Scan all files
-    $files = self::array_flatten(Kohana::list_files('', array(DOCROOT)));
+		// Scan all files
+		$files = self::array_flatten(Kohana::list_files('', array(DOCROOT)));
 
-    $archive_file = APPPATH.'cache/'.$this->prefix.'files-'.date('Y-m-d-H.i.s').'.tar.gz';
-    $archive = new Archive_Tar($archive_file, 'gz');
-    $archive->create($files);
+		$archive_file = APPPATH.'cache/'.$this->prefix.'files-'.date('Y-m-d-H.i.s').'.tar.gz';
+		$archive = new Archive_Tar($archive_file, 'gz');
+		$archive->create($files);
 
-    // Send file to download
-    $this->response->send_file($archive_file);    
+		// Send file to download
+		$this->response->send_file($archive_file);
 	}
 
-  protected function array_flatten($array, $return = array())
-  {
-    foreach ($array as $value)
-    {
-      if (is_array($value))
-      {
-        $return = self::array_flatten($value, $return);
-      }
-      else
-      {
-        $return[] = $value;
-      }
-    }
+	protected function array_flatten($array, $return = array())
+	{
+		foreach ($array as $value)
+		{
+			if (is_array($value))
+			{
+				$return = self::array_flatten($value, $return);
+			}
+			else
+			{
+				$return[] = $value;
+			}
+		}
 
-    return $return;
-  }
+		return $return;
+	}
 
 }
