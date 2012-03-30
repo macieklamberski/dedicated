5c09b7d3438de3a5221d0f6f8c1f53163a741fcc
Maciek Lamberski <maciej@lamberski.com>
2012-03-31 00:16:07 +0300

    Remove project-specific options

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-echo 'nagusame';
+
 /**
  * Set Kohana::$environment if a 'KOHANA_ENV' env variable has been supplied.
  */
@@ -14,46 +14,22 @@ if (isset($_SERVER['KOHANA_ENV']))
 Kohana::$base_url = preg_replace('/.*RewriteBase\s([^\n]+).*/s', '$1', file_get_contents('.htaccess'));
 
 /**
- * Initialize Kohana, setting the default options.
+ * Set default Kohana options.
  */
 switch (Kohana::$environment)
 {
 	case Kohana::DEVELOPMENT:
 	case Kohana::TESTING:
-		Kohana::init(array(
-			'base_url'   => Kohana::$base_url,
-			'index_file' => FALSE,
-			'profiling'  => TRUE,
-			'caching'    => FALSE,
-		));
+		Kohana::$index_file = FALSE;
+		Kohana::$profiling = TRUE;
+		Kohana::$caching = FALSE;
 	break;
 	case Kohana::PRODUCTION:
 	default:
-		Kohana::init(array(
-			'base_url'   => Kohana::$base_url,
-			'index_file' => FALSE,
-			'profiling'  => FALSE,
-			'caching'    => TRUE,
-		));
+		Kohana::$index_file = FALSE;
+		Kohana::$profiling = FALSE;
+		Kohana::$caching = TRUE;
 	break;
-}
-
-/**
- * Attach the file write to logging. Multiple writers are supported.
- */
-Kohana::$log->attach(new Log_File(APPPATH.'logs'));
-
-/**
- * Attach a file reader to config. Multiple readers are supported.
- */
-Kohana::$config->attach(new Config_File);
-
-/**
- * Attach e-mail logging module on testing and production environment.
- */
-if (Kohana::$environment != Kohana::DEVELOPMENT)
-{
-	Kohana::$log->attach(new Log_Watchy(Arr::get($_SERVER['SERVER_NAME']), array('maciej@lamberski.com')), Log::CRITICAL);
 }
 
 /**
