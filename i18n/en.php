e5c586cab20bbb5bd881305316ae34a4e5589660
Maciej Lamberski <maciej@lamberski.com>
2012-03-30 21:39:48 +0200

    Update translations

--- i18n/en.php
+++ i18n/en.php
@@ -1,13 +1,16 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
+	// Common
+	'admin.lang_tabs.hint'      => 'The fields below correspond to the selected language. Please complete all required fields in each of the available languages.',
+
 	// Manageable
 	'admin.model.added'          => ':model successfully added.',
 	'admin.model.updated'        => ':model successfully updated.',
 	'admin.model.deleted'        => ':model successfully deleted.',
 
 	// Dashboard
-	'admin.welcome'              => 'Welcome in :project_name admin panel!',
+	'admin.welcome'              => 'Welcome in admin panel!',
 
 	// Settings
 	'admin.settings.updated'     => 'Settings saved.',
