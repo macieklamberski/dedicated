572b9de82a3c59a246de561c368c6309c2b95171
Maciej Lamberski <maciej@lamberski.com>
2012-04-15 12:52:16 +0200

    Remove all Kohana-related settings

--- config/cmf.php
+++ config/cmf.php
@@ -1,7 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'route_prefix' => 'cms',
 	'modules' => array(
 		'multilanguage' => TRUE,
 		'i18n' => TRUE,
--- init.php
+++ init.php
@@ -3,54 +3,15 @@
 /**
  * Define customized __() function.
  */
-function __($string, array $values = NULL, $lang = 'en')
+if ( ! function_exists('__'))
 {
-	if ($lang !== I18n::$lang)
+	function __($string, array $values = NULL, $lang = 'en')
 	{
-		$string = I18n::get($string);
+		if ($lang !== I18n::$lang)
+		{
+			$string = I18n::get($string);
+		}
+
+		return empty($values) ? $string : strtr($string, $values);
 	}
-
-	return empty($values) ? $string : strtr($string, $values);
-}
-
-/**
- * Set Kohana::$environment if a 'KOHANA_ENV' env variable has been supplied.
- */
-if (isset($_SERVER['KOHANA_ENV']))
-{
-	Kohana::$environment = constant('Kohana::'.strtoupper($_SERVER['KOHANA_ENV']));
-}
-
-/**
- * Setting base_url from RewriteBase directive placed in .htaccess.
- */
-Kohana::$base_url = preg_replace('/.*RewriteBase\s([^\n]+).*/s', '$1', file_get_contents('.htaccess'));
-
-/**
- * Set default Kohana options.
- */
-switch (Kohana::$environment)
-{
-	case Kohana::DEVELOPMENT:
-	case Kohana::TESTING:
-		Kohana::$index_file = FALSE;
-		Kohana::$profiling = TRUE;
-		Kohana::$caching = FALSE;
-	break;
-	case Kohana::PRODUCTION:
-	default:
-		Kohana::$index_file = FALSE;
-		Kohana::$profiling = FALSE;
-		Kohana::$caching = TRUE;
-	break;
-}
-
-/**
- * Load routes from external config files.
- */
-if ( ! Route::cache())
-{
-	Kohana::$config->load('routes');
-
-	Route::cache(Kohana::$environment == Kohana::PRODUCTION);
 }
