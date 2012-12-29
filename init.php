fb81723a6ab8adc448169debf261b8f1949083ec
Maciek Lamberski <maciej@lamberski.com>
2012-12-29 14:34:36 +0100

    Fix bug with not getting translation when default $lang

--- init.php
+++ init.php
@@ -7,10 +7,7 @@ if ( ! function_exists('__'))
 {
 	function __($string, array $values = NULL, $lang = 'en')
 	{
-		if ($lang !== I18n::$lang)
-		{
-			$string = I18n::get($string);
-		}
+		$string = I18n::get($string);
 
 		return empty($values) ? $string : strtr($string, $values);
 	}
